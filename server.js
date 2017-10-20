a80bd5652267fb80deb714f9eaa7c1d63ad07f36
Prabhjot Singh Sidhu <32662975+prabhjotsingh0000@users.noreply.github.com>
2017-10-20 13:20:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,52 @@
+/**
+ * Created by prabhjot on 8/3/17.
+ */
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.Server(app);
+const socket = require('socket.io');
+
+const io = socket(server);
+
+let chats = [];
+let clients = {};
+
+io.on("connection",function (conn) {
+    console.log("A client has connected" + conn.id);
+
+    conn.emit("chatlog", chats);
+
+    conn.on("sign_up", function (data) {
+        clients[data] = conn.id;
+        console.log(clients);
+    });
+
+    conn.on("send_msg", function (data) {
+        if (data.msg.charAt(0) == "@") {
+            let toUser = data.msg.split(" ", 1)[0].substring(1);
+            let msg = data.msg.substring(data.msg.indexOf(" ") + 1);
+
+            io.to(clients[toUser]).emit("rcv_msg", data.user + ": " + msg);
+        } else {
+            chats.push(data);
+            io.emit("rcv_msg", data.user + ": " + data.msg)
+            //conn.broadcast.emit("rcv_msg",data)
+
+        }
+    });
+
+    conn.on("disconnect", function (data) {
+        console.log("A user disconnected");
+    })
+
+});
+
+
+app.use('/', express.static(__dirname + "/public"));
+
+
+server.listen(3456, function () {
+    console.log('Server started');
+});
